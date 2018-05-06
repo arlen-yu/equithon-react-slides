c7e77fda070d78e56389affb77bf7685b7cbac82
Arlen Yu <arlen.yu6@gmail.com>
2018-05-05 21:06:25 -0400

    Updated slides

--- src/presentation.js
+++ src/presentation.js
@@ -79,6 +79,12 @@ export default class Presentation extends React.Component {
         <Slide
           transition={['slide']}
           bgColor='secondary'
+          notes={
+            <ul>
+              <li>Did ya'll notice reddit was looking different</li>
+              <li>Pinterest gestalt</li>
+            </ul>
+          }
         >
           <Heading size={6} textColor='primary'>A brief history</Heading>
           <List textColor='primary'>
@@ -112,9 +118,28 @@ export default class Presentation extends React.Component {
         >
           <Heading size={1} textColor='secondary'>So what is it exactly? 🤔</Heading>
         </Slide>
-        <Slide transition={['slide']} bgColor='secondary' textColor='primary'>
-          <List  textColor='primary'>
-            <ListItem textSize={34}>A javascript library for building user interfaces</ListItem>
+        <Slide
+          bgColor='secondary'
+          textColor='primary'
+          notes={
+          <ul>
+            <li>
+              React is JUST THE VIEW LAYER (more or less)
+              <ul><li>Based on this idea: what if you could create your own HTML element that has custom functionality</li></ul>
+            </li><br />
+            <li>
+              Components manage their own states, and then are composed together. They're self contained, and composable. For example, take a phone.
+              <ul>
+                <li>
+                  This is a huge plus of React. It makes your code extremely reusable, and a lot easier to debug.
+                </li>
+              </ul>
+            </li>
+          </ul>
+          }
+        >
+          <List textColor='primary'>
+            <ListItem textSize={34}>An open source javascript library for building user interfaces</ListItem>
             <Appear order={2}><ListItem textSize={34}>Splits complex user interfaces into simple components</ListItem></Appear>
             <Appear order={3}><ListItem textSize={34}>Pioneered the virtual DOM</ListItem></Appear>
             <Appear order={4}><ListItem textSize={34}>Introduced JSX</ListItem></Appear>
@@ -123,8 +148,29 @@ export default class Presentation extends React.Component {
         <Slide>
           <Heading size={1} fit textColor='secondary'>Virtual DOM</Heading>
         </Slide>
-        <Slide transition={['slide']} bgColor='secondary' textColor='primary'>
-          <List  textColor='primary'>
+        <Slide
+          bgColor='secondary'
+          textColor='primary'
+          notes={
+            <ul>
+              <li>DOM is a programming interface for HTML and XML documents. It represents a page so that it can be modified.</li><br />
+              <li>
+                Manipulating the DOM can be really slow sometimes.
+                <ul>
+                  <li>For example, checking off an item in a list will often rerender the whole list</li>
+                  <li>This slowness has a huge impact, especially in modern web applications where the DOM is being manupulated constantly</li>
+                </ul>
+              </li><br />
+              <li>
+                Virtual DOM is fast.
+                <ul>
+                  <li>Once the virtual DOM is updated, it compares it with the snapshot of the virtual DOM before the update and identifies exactly what has changed</li>
+                </ul>
+              </li>
+            </ul>
+          }
+        >
+          <List textColor='primary'>
             <ListItem>DOM is the Document Object Model; a tree-like structure for your document</ListItem>
             <Appear order={2}><ListItem>Virtual DOM is a representation of the DOM</ListItem></Appear>
             <Appear order={3}><ListItem>Manipulating the DOM? Slow. Manipulating the Virtual DOM? Fast ⚡</ListItem></Appear>
@@ -132,12 +178,13 @@ export default class Presentation extends React.Component {
           </List>
         </Slide>
         <Slide
-          transition={['slide']}
           bgColor='primary'
           textColor='secondary'
-          notes={`
-          Stay on this slide for a while. Build up the narrative that people hated JSX when it first came out.
-          `}
+          notes={
+            <ul>
+              <li>People really didn't like JSX when it first came out. It was viewed at as anti-pattern, and seen as a horrendous intermingle of HTML and JS</li>
+            </ul>
+          }
         >
           <Heading size={1} fit textColor='secondary'>WTF is JSX</Heading>
         </Slide>
@@ -150,15 +197,32 @@ export default class Presentation extends React.Component {
           </BlockQuote>
         </Slide>
         <Slide
-          notes={`
-          This seemed like a great idea. Our HTML is totally pure! But then we realized some problems: Uh, how can I tell that these two lines are interconnected? Answer: I can’t unless I read every single line of JavaScript. With this pattern, you can’t change a line of markup without checking every single line of JavaScript to assure you’re not breaking a selector. You see, there’s no actual separation going on here. Sure, the JS and HTML are in separate files, but these two technologies are fundamentally joined at the hip. They must move in lockstep or the application will crash. Strictly separating HTML and JS actually led to applications that were harder to maintain and debug. Each time you wanted to change a line of markup, you had to worry about breaking a jQuery selector. Perhaps if we relaxed our religious devotion to separation of concerns, we could relieve some of this pain? This ushered in phase 2…`}
+          notes={
+            <ul>
+              <li>At first, this is how JavaScript and interacted. Completely Seperate.</li><br />
+              <li>
+                There's a few problems with this. How can I tell what is connected with what?
+                <ul>
+                  <li>You can't really. Unless you read every single line, a pain in the ass.</li>
+                  <li>Essentially completely seperate but only semantically, you still have to treat them as a collection if you want to make changes to the code.</li>
+                </ul>
+              </li><br />
+            </ul>
+          }
         >
           <Heading size={6}>The Good Old Days: Unobtrusive Javascript</Heading>
           <Layout>
             <Fill><Image src={'images/unobtrusiveJS.png'} /></Fill>
           </Layout>
         </Slide>
-        <Slide>
+        <Slide
+          notes={
+            <ul>
+              <li>A marriage of HTML and JS, living in harmony</li>
+              <li>Rather than putting JS in HTML, React treats in more as putting HTML in JS</li>
+            </ul>
+          }
+        >
           <Heading size={6}>Using JSX:</Heading>
           <Layout>
             <Fill><Image src={'images/withJSX.png'} /></Fill>
@@ -171,11 +235,51 @@ export default class Presentation extends React.Component {
         </Slide>
         <Slide
           bgColor='primary'
+          notes={
+            <ul>
+              <li>
+                Key things to note:
+                <ul>
+                  <li>This is one way to write components which uses ES6 class notation</li>
+                  <li>Class components have a function called render which returns the JSX of the component</li>
+                  <li>They have a lot more special functions called "lifecycle functions" which we'll touch on later</li>
+                </ul>
+              </li>
+            </ul>
+          }
         >
           <Heading size={6} textColor='secondary'>A Simple Component</Heading>
           <Layout>
             <Fill><Image src={'images/simpleExample.png'} /></Fill>
           </Layout>
+        </Slide>
+        <Slide
+          bgColor='primary'
+          notes={
+            <ul>
+              <li>Put simply, they're parameters for customizing your components</li>
+              <li>Is also a key proponent to what makes our components reusable (gestalt)</li>
+            </ul>
+          }
+        >
+          <Heading size={6} textColor='secondary'>Props (short for properties)</Heading>
+          <List textColor='secondary'>
+            <Appear order={1}><ListItem textSize={34}>"Parameters" you can pass to your components</ListItem></Appear>
+            <Appear order={2}><ListItem textSize={34}>Accessible through a this.props object</ListItem></Appear>
+            <Appear order={3}><ListItem textSize={34}>Data flows downwards via props</ListItem></Appear>
+            <Appear order={3}><ListItem textSize={34}>Props are immutable</ListItem></Appear>
+          </List>
+        </Slide>
+        <Slide bgColor='primary'>
+          <Heading size={6} textColor='secondary'>State</Heading>
+          <List textColor='secondary'>
+            <Appear order={1}><ListItem textSize={34}>Managed within a Component</ListItem></Appear>
+            <Appear order={2}><ListItem textSize={34}>Used for mutable data</ListItem></Appear>
+            <Appear order={3}><ListItem textSize={34}>Use setState to update a components state</ListItem></Appear>
+          </List>
+        </Slide>
+        <Slide bgColor='primary'>
+          <Heading size={6} textColor='secondary'>https://github.com/arlen-yu/equithon-react-boilerplate</Heading>
         </Slide>
         <Slide bgColor='primary'>
           <Image src='images/tree1.png' />
@@ -197,7 +301,7 @@ export default class Presentation extends React.Component {
           bgColor='primary'
           textColor='secondary'
         >
-          <Heading size={6} fit caps>key design tip #1</Heading>
+          <Heading size={6} fit caps>key design tip</Heading>
         </Slide>
         <Slide
           bgColor='primary'
@@ -228,56 +332,6 @@ export default class Presentation extends React.Component {
             <Appear order={2}><ListItem textSize={34}>Provide data to other components</ListItem></Appear>
             <Appear order={3}><ListItem textSize={34}>Have state</ListItem></Appear>
           </List>
-        </Slide>
-        <Slide
-          bgColor='secondary'
-          textColor='primary'
-        >
-          <Heading size={6} textColor='primary' fit caps>key design tip #2</Heading>
-        </Slide>
-        <Slide
-          bgColor='secondary'
-          textColor='primary'
-        >
-          <Heading size={6} textColor='primary' caps>avoid closures, use handlers</Heading>
-        </Slide>
-        <Slide
-          bgColor='secondary'
-          textColor='primary'
-        >
-          <Heading size={6} textColor='primary'>Don't do this</Heading>
-          <CodePane
-            lang={'jsx'}
-            source={`
-<SomeComponent
-  onChange={(e) => {
-    e.preventDefault()
-    console.log('something')
-  }}
-  onSubmit={(e) => {
-    const yes = 'something' + 'else'
-    console.log(yes)
-  }}
-  reallyComplexLogicFunc={() => {
-    // complex
-  }}
-/>`}
-          />
-        </Slide>
-        <Slide
-          bgColor='secondary'
-          textColor='primary'
-        >
-          <Heading size={6} textColor='primary'>Do this</Heading>
-          <CodePane
-            lang={'jsx'}
-            source={`
-<SomeComponent
-  onChange={this.handleOnChange}
-  onSubmit={this.handleOnSubmit}
-  reallyComplexLogicFunc={this.handleReallyComplexLogicFunc}
-/>`}
-          />
         </Slide>
         <Slide
           bgColor='primary'
